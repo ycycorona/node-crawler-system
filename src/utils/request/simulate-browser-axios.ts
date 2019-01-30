6d76a2ce2572483c56e5a3c5785989f505f5717e
ycycorona <ycy421@gmail.com>
2019-01-31 01:14:34 +0800

    编译后如果用非相对路径引入模块 需要定义全局变量

--- src/utils/request/simulate-browser-axios.ts
+++ src/utils/request/simulate-browser-axios.ts
@@ -1,8 +1,7 @@
 import axiosCreator from './axios_creator.js'
 import {IAxiosOpts} from 'common/interface.ts'
-
-const simulateBrowser = require('./plugins/simulate-browser')
-const interHandleError = require('./interceptors/handle_error')
+import simulateBrowser from './plugins/simulate_browser'
+import interHandleError from './interceptors/handle_error'
 const options: IAxiosOpts = {
   headers: simulateBrowser().headers,
   interceptors: [interHandleError]
