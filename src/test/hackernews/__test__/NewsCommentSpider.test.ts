2c4916fda050c2734452cd4224ce35c055a41250
yichenyang <yichenyang@mobanker.com>
2019-02-11 10:45:26 +0800

    添加.vscode路径到git

--- src/test/hackernews/__test__/NewsCommentSpider.test.ts
+++ src/test/hackernews/__test__/NewsCommentSpider.test.ts
@@ -11,14 +11,14 @@ let newsCommentSpider: NewsCommentSpider = new NewsCommentSpider().setRequest(
 .setExtra({storyId: 19113635})
 
 describe('NewsCommentSpider', function() {
-  it('run', async function() {
+  it.only('run', async function() {
     // 不执行持久化操作
     let res: any = await newsCommentSpider.run(false)
 
     assert.ok(res.commentsList instanceof Array && res.commentsList.length > 0)
     console.log(res.commentsList.length)
   })
-  it.only('simple test', async function(){
+  it('simple test', async function(){
     console.log(newsCommentSpider instanceof NewsCommentSpider)
     console.log(newsCommentSpider instanceof Spider)
   })
